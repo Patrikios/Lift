4ddb8219a814b4822e8f3d96d8d1ae0c07771368
Patrik Punco <puncopatrik@gmail.com>
2019-01-27 22:06:12 +0100

    Cleaned up code, removed cuttof overhead calculation

--- giveMeLift.R
+++ giveMeLift.R
@@ -1,42 +1,43 @@
-#lift calculation
+#lift calculation for binary response
 
 liftMe <- function(
-  dataWithProbabilityPrediction = xgbpred2, 
-  responseVariable = "Churn", 
+  dataWithProbabilityPrediction = pred,
+  levelPositive = "yes",
+  responseVariable = "y", 
   probabilityOfChurning = "pred"
-  #cutpoint = 0.2 #cutppoint for creating a prediciton
 ) 
 {
+  lapply(c("data.table", "ggplot2"), 
+         function(pak) {
+          if(!pak %in% .packages()) if(!suppressWarnings(suppressPackageStartupMessages(require(pak, character.only = T)))) { 
+                                            install.packages(pak); library(pak,  character.only = T)} 
+                        }
+         )
   
-  if(!require(data.table)) { install.packages("data.table"); library(data.table)}
-  if(!require(ggplot2)) { install.packages("ggplot2"); library(ggplot2)}
-  if(!require(lift)) { install.packages("lift"); library(lift)}
+  #probability data table and its proxy
+  dataWithProbabilityPrediction <- data.table(label = dataWithProbabilityPrediction[[responseVariable]], 
+                                              probabilityOfEvent = dataWithProbabilityPrediction[[probabilityOfChurning]]
+                                              )
+  DWP = dataWithProbabilityPrediction #define DWP as shallow copy of (or in other words a proxy to) dataWithProbabilityPrediction
+  levelNegative <- unique(DWP$label) [! unique(DWP$label) %in% levelPositive]
   
-  #probability data table
-  probabilityDT <- data.table(label = dataWithProbabilityPrediction[[responseVariable]], 
-                              probabilityOfChurning = dataWithProbabilityPrediction[[probabilityOfChurning]])
-  #probabilityDT[, predictionChurn := 0]
-  #probabilityDT[probabilityOfChurning > 0.2 , predictionChurn := 1]
-  #probabilityDT[, predictionChurn := as.factor(predictionChurn)]
-  setorderv(x = probabilityDT, cols = "probabilityOfChurning", order = -1)
+  setorderv(x = DWP, cols = "probabilityOfEvent", order = -1)
   
-  #Churn rate in the testing data set
-  ChurnRate <- prop.table(table(dataWithProbabilityPrediction[[responseVariable]]))['1'] #churn rate in dataWithProbabilityPrediction aka data we predict on
-  print(paste("Churn rate in Test Datensatz ist", scales::percent(ChurnRate), sep = " "))
-  print(table(probabilityDT$label))
+  #rate of the positive event in the dataWithProbabilityPrediction aka the test data set
+  PRE <- prop.table(table(DWP[["label"]]))[levelPositive] #PRE is rate of the positive event in DPW data.table proxy
+  print(paste("Churn rate in Test Datensatz ist", scales::percent(PRE), sep = " "))
+  print(table(DWP$label))
   cat("\n")
   CrossTab <- function(data) with(data, table(label = label))
-  depth <- function(quantile = 1) floor(quantile/100*nrow(dataWithProbabilityPrediction))
-  createQuantileDT <- function(quantile = 1) { #aka 1% depth of data
-    crossed <- CrossTab(probabilityDT[1:depth(quantile)])
-    FP <- crossed['0']
+  depth <- function(quantile = 1) floor(quantile/100*nrow(DWP))
+  createQuantileDT <- function(quantile = 1) { #1 aka 1% depth of data
+    crossed <- CrossTab(DWP[1:depth(quantile)])
+    FP <- crossed[levelNegative]
     FP <- ifelse(is.na(FP), 0, FP)
-    TP <- crossed['1']
+    TP <- crossed[levelPositive]
     TP <- ifelse(is.na(TP), 0, TP)
-    TP_FP_Ratio <- if(FP == 0 || TP == 0) "-" else paste0('1:', round(FP/TP)) 
-    Lift <- if(TP != 0) 
-      round(prop.table(table(probabilityDT[1:depth(quantile)]$label))['1']/ChurnRate, 3) 
-    else ("-")
+    TP_FP_Ratio <- if(FP == 0 || TP == 0) "-" else paste0('1:', round(FP/TP, 1))
+    Lift <- if(TP != 0) round(prop.table(table(DWP[1:depth(quantile), label]))[levelPositive]/PRE, 3) else "-"
     return(data.table(FP = FP, TP = TP,  TP_FP_Ratio = TP_FP_Ratio, Lift = Lift))
   }
   
@@ -47,19 +48,19 @@ liftMe <- function(
   lifts[, depth := lapply(quantiles, depth)]
   setcolorder(lifts, c("quantile", "depth", "FP", "TP", "TP_FP_Ratio", "Lift"))
   print(lifts)
-  cat("\n")
-  print(paste0("Top Decile: ", TopDecileLift(predicted = probabilityDT$probabilityOfChurning, labels = probabilityDT$label)))
   
   LiftVec <- vector(mode = "integer", length = 100)
   for (i in 1:100) {
-    p <- prop.table(table(probabilityDT[1:depth(i)]$label))['1']/ChurnRate
+    p <- prop.table(table(DWP[1:depth(i), label]))[levelPositive]/PRE
     LiftVec[i] <- p
   }
-  gg <- ggplot(data = data.frame(Quantil = 1:100, Lift = LiftVec)) +
-    geom_line(aes(x = Quantil, y = Lift, color = 'orange'), size=1) +
+  
+  #Plot Lift curve
+  gg <- ggplot(data = data.table(Quantil = 1:100, Lift = LiftVec)) +
+    geom_line(aes(x = Quantil, y = Lift, color = "orange"), size = 1) +
     geom_text(aes(label = round(Lift, 2), x = Quantil, y = Lift, color = 'orange'), hjust = -1, size = 2) +
     theme_minimal() +
-    ggtitle("Kumulatives Lift") +
+    ggtitle("Kumulative Lift") +
     theme(plot.title = element_text(hjust = 0.5)) +
     theme(legend.position="none")
   print(gg)
